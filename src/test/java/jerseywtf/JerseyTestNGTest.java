718bdfa18e36b8323a0e913aca5d65de41daad4f
robert.kuhar <robert.kuhar@climate.com>
2014-10-31 16:59:38 -0700

    Initial checkin

---
+++ src/test/java/jerseywtf/JerseyTestNGTest.java
@@ -0,0 +1,40 @@
+package jerseywtf;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTestNg;
+import org.testng.annotations.Test;
+
+public class JerseyTestNGTest extends JerseyTestNg.ContainerPerClassTest {
+
+  @Path("hello")
+  public static class HelloResource {
+    @GET
+    public String getHello() {
+      return "Hello World!";
+    }
+  }
+
+  @Override
+  protected Application configure() {
+    return new ResourceConfig(HelloResource.class);
+  }
+
+  @Test
+  public void notIntegrationTest() {
+    final String hello = target("hello").request().get(String.class);
+    assertEquals("Hello World!", hello);
+  }
+
+  @Test(groups = {"integration"})
+  public void integrationTest() {
+    final String jello = target("hello").request().get(String.class);
+    assertEquals("Hello World!", jello);
+  }
+
+}
